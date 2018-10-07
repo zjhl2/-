81a1d9e40bd39605f731e0abcbe3d4616bc5eb9a
zjhl2 <478791469@qq.com>
2018-10-07 21:55:35 +0800

    fft

--- 数学/fft.cpp
+++ 数学/fft.cpp
@@ -1,128 +1,92 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int N=400005;
-const double PI=acos(-1);
-struct Complex{
-    double x, y;
-    Complex(double x = 0.0, double y = 0.0) : x(x), y(y){}
-    Complex operator + (const Complex &b) const{
-        return Complex(x + b.x, y + b.y);
-    }
-    Complex operator - (const Complex &b) const{
-        return Complex(x - b.x, y - b.y);
-    }
-    Complex operator * (const Complex &b) const{
-        return Complex(x * b.x - y * b.y, x * b.y + y * b.x);
-    }
-};
+typedef long long ll;
+typedef long double ldb;
+typedef double db;
 
-Complex x1[N], x2[N];
 
-void change(Complex y[], int len){
-    for (int i = 1, j = len / 2; i < len - 1; i++){
-        if (i < j) swap(y[i], y[j]);
-        int k = len / 2;
-        while (j >= k){
-            j -= k;
-            k /= 2;
-        }
-        if (j < k) j += k;
+namespace fft{ 
+    const int mod = 998244353;
+    const int L = 20, FN = (1<<L);
+    int rev[FN];   
+    const db pi = acos(-1);
+    const db two_pi = pi * 2;
+    struct cp {
+        db x, y;
+        cp() {}
+        cp(db _x) : x(_x), y(0) {}
+        cp(db _x, db _y) : x(_x), y(_y) {}
+        inline cp operator + (const cp &p) const { return cp(x+p.x, y+p.y); }
+        inline cp operator - (const cp &p) const { return cp(x-p.x, y-p.y); }
+        inline cp operator * (const cp &p) const { return cp(x*p.x-y*p.y, x*p.y+y*p.x); }
+        inline cp operator !() const { return cp(x, -y); }
+    } nw[FN+1], f[FN], g[FN], h[FN], hh[FN];
+    void init() {
+        rev[0] = 0; for (int i = 1; i < FN; i ++) rev[i] = (rev[i>>1]>>1)|((i&1)<<(L-1));
+        nw[0] = nw[FN] = cp(1,0);
+        for (int i = 1; i <= (FN>>1); i ++) nw[i] = cp(cos(i*two_pi/FN), sin(i*two_pi/FN));
+        for (int i = (FN>>1)+1; i < FN; i ++) nw[i] = !nw[FN-i];
     }
-}
-
-void fft(Complex y[], int len, int on){
-    change(y, len);
-    for (int h = 2; h <= len; h <<= 1){
-        Complex wn(cos(-on * 2 * PI / h), sin(-on * 2 * PI / h));
-        for (int j = 0; j < len; j += h){
-            Complex w(1, 0);
-            for (int k = j; k < j + h / 2; k++){
-                Complex u = y[k];
-                Complex t = w * y[k + h / 2];
-                y[k] = u + t;
-                y[k + h / 2] = u - t;
-                w = w * wn;
+    void dft(cp *a, int n, int fg) {
+        int d = __builtin_ctz(FN / n);
+        for (int i = 0; i < n; i ++) if (i < (rev[i]>>d)) swap(a[i], a[rev[i]>>d]);
+        for (int size = 2; size <= n; size <<= 1) {
+            int step = FN / size; if (fg) step = -step;
+            for (int i = 0; i < n; i += size) {
+                cp *u = a + i, *v = a + (i+(size>>1)), *w = fg ? nw + FN : nw;
+                for (int k = (size>>1); k --; ) {
+                    cp tmp = (*v) * (*w);
+                    *v = *u - tmp, *u = *u + tmp;
+                    u ++, v ++, w += step;
+                }
             }
+        } 
+        if (fg) for (int i = 0; i < n; i ++) a[i].x /= n, a[i].y /= n;
+    }
+    /// multiply two polynomial nlog(n) time modulo p
+    void multiply(int *a, int *b, int *c, int n) {
+        for (int i = 0; i < n; i ++) {
+            f[i] = cp( (db)(a[i]>>15), (db)(a[i]&32767) );
+            g[i] = cp( (db)(b[i]>>15), (db)(b[i]&32767) );
         }
+        dft(f, n, 0);
+        dft(g, n, 0);
+        for (int i = 0; i < n; i ++) {
+            int j = i ? n - i : 0;
+            hh[i] = ((f[i]+!f[j])*(!g[j]-g[i]) + (!f[j]-f[i])*(g[i]+!g[j])) * cp(0, 0.25);
+            h[i] = (!f[j]-f[i])*(!g[j]-g[i])*cp(-0.25, 0) + (f[i]+!f[j])*(g[i]+!g[j])*cp(0, 0.25);
+        }
+        dft(h, n, 1);
+        dft(hh, n, 1);
+        for (int i = 0; i < n; i ++) 
+            c[i] = (ll(h[i].x + 0.5) + (ll(hh[i].x + 0.5) % mod << 15) + (ll(h[i].y + 0.5) % mod<<30)) % mod;
     }
-
-    if (on == -1){
-        for (int i = 0; i< len; i++) y[i].x /= len;
+    void mul(int *a, int *b,int *c,int n) {
+        for (int i = 0; i < n; i ++) 
+            f[i] = cp(a[i]), g[i] = cp(b[i]);
+        dft(f, n, 0);
+        dft(g, n, 0);
+        for (int i = 0; i < n; i ++) f[i] = f[i] * g[i];
+        dft(f, n, 1);
+        for (int i = 0; i < n; i ++) c[i] = int(f[i].x + 0.5);
+    }
+    //[0, n) * [0, m) = [0, n + m - 2]
+    int getlen(int n, int m){
+        int len = 1;
+        while(len < n + m - 1) len <<= 1;
+        return len;
     }
 }
 
-int mul(int p[], int dp, int q[], int dq){
-    int len = 1;
-    while (len < dp + dq - 1) len <<= 1;
-
-    for (int i = 0; i < len; i++)
-    {
-        if (i < dp) x1[i] = Complex(p[i], 0);
-        else x1[i] = Complex(0, 0);
-        if (i < dq) x2[i] = Complex(q[i], 0);
-        else x2[i] = Complex(0, 0);
-    }
-
-    fft(x1, len, 1);
-    fft(x2, len, 1);
-
-    for (int i = 0; i < len; i++)
-        x1[i] = x1[i] * x2[i];
-
-    fft(x1, len, -1);
-
-    for (int i = 0 ;i < len; i++)
-        p[i] = (int)(x1[i].x + 0.5);
-
-    return len;
-}
-int arr1[N],arr2[N];
-
-
-
-#include<bits/stdc++.h>
-#define pi acos(-1)
-using namespace std;
-typedef complex<double> E;
 const int N=400005;
-int n,m;  //10w
-E a[N],b[N];
-int c[N];
-void FFT(E *x,int n,int type)
-{
-    if(n==1)return;
-    E l[n>>1],r[n>>1];
-    for(int i=0;i<n;i+=2)
-        l[i>>1]=x[i],r[i>>1]=x[i+1];
-    FFT(l,n>>1,type);FFT(r,n>>1,type);
-    E wn(cos(2*pi/n),sin(type*2*pi/n)),w(1,0);
-    for(int i=0;i<n>>1;i++,w*=wn)
-        x[i]=l[i]+w*r[i],x[i+(n>>1)]=l[i]-w*r[i];
-}
-
-char s1[N],s2[N];
-int main()
-{
-    while(~scanf("%s%s",s1,s2))
-    {
-        int n=strlen(s1),m=strlen(s2);
-        for (int i=0;i<n;i++) a[i]=s1[n-1-i]-'0';
-        for (int i=0;i<m;i++) b[i]=s2[m-1-i]-'0';
-        m+=n; for (n=1;n<m;n<<=1);
-        FFT(a,n,1); FFT(b,n,1);
-        for (int i=0;i<n;i++) a[i]*=b[i];
-        FFT(a,n,-1);
-        int top=0;
-        for (int i=0;i<m;i++)
-        {
-            c[i]+=int(a[i].real()/n+0.5);
-            c[i+1]+=c[i]/10;
-            c[i]%=10;
-            if (c[i]>0) top=i;
-        }
-        for (int i=top;i>=0;i--) printf("%d",c[i]);
-        printf("\n");
-        for (int i=0;i<n;i++) a[i]=b[i]=c[i]=0;
-    }
+int arr1[N],arr2[N],arr3[N];
+int main(){
+    fft::init();
+    int n,m; scanf("%d%d",&n,&m);
+    n++; m++;
+    for (int i=0;i<n;i++) scanf("%d",&arr1[i]);
+    for (int i=0;i<m;i++) scanf("%d",&arr2[i]);
+    fft::multiply(arr1,arr2,arr3,fft::getlen(n,m));
+    for (int i=0;i<=n+m-2;i++) printf("%d ",arr3[i]);
 }
